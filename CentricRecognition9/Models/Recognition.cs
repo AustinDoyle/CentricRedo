caf08ea7408eb3a5357060fbf6663a24a9cadce4
Austin <ad567613@ohio.edu>
2017-12-09 17:24:23 -0500

    Fullname and Layout
    Breaks for layout
    Full name works in recognition

--- CentricRecognition9/Models/Recognition.cs
+++ CentricRecognition9/Models/Recognition.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -14,6 +15,7 @@ namespace CentricRecognition9.Models
         public string description { get; set; }
 
         public Guid EmployeeID { get; set; }
+        [ForeignKey("EmployeeID")]
 
         public virtual Employee Employee { get; set; }
 
